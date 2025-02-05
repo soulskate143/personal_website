bccfe579fabb68f989f6c6d60613d9824e84edee
soulskate143 <52129577+soulskate143@users.noreply.github.com>
2025-02-05 19:27:12 +0800

    Update project.jsx

--- Components/project.jsx
+++ Components/project.jsx
@@ -15,7 +15,8 @@ const softwareProjects = [
   {
     title: "Stars",
     description: "A comprehensive digital platform designed to monitor, analyze, and optimize maritime operations in real time",
-    description2: "A comprehensive digital solution that integrates advanced technologies to monitor, analyze, and optimize maritime activities in real time. Designed for ports, shipping companies, and logistics operators, the platform enhances safety, efficiency, and compliance across maritime ecosystems.",
+    description2: "A comprehensive digital solution that integrates advanced technologies to monitor, analyze, and optimize maritime activities in real time. Designed for ports, shipping companies, and logistics operators, the platform enhances safety, efficiency, and compliance across maritime ecosystems. Visit ",
+    url: "https://stars.avegabros.org",
     images: ["/stars/1.png", "/stars/2.png", "/stars/3.png", "/stars/4.png", "/stars/5.png", "/stars/6.png", 
       "/stars/7.png", "/stars/8.png", "/stars/9.png", "/stars/10.png", "/stars/11.png", "/stars/12.png", "/stars/13.png"],
   },
